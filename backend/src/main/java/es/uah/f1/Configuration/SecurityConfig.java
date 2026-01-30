b54bf1ff8dd074bece7e6edcba6b484440375a18
IvanPerez <64197858+ivanperez-c@users.noreply.github.com>
2026-01-30 08:46:56 +0100

    Arreglar cors back, fix error circuitos front

--- backend/src/main/java/es/uah/f1/Configuration/SecurityConfig.java
+++ backend/src/main/java/es/uah/f1/Configuration/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +19,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // Deshabilita protección CSRF para pruebas Postman
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
+                .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll() // Permite todo lo que empiece por /api/
-                        .anyRequest().permitAll() // O permite absolutamente todo
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- frontend/src/app/features/public/calendar/calendar.ts
+++ frontend/src/app/features/public/calendar/calendar.ts
@@ -19,7 +19,7 @@ export class Calendar implements OnInit {
   constructor(private circuitsService: CircuitService) {}
 
   ngOnInit(): void {
-    this.circuitsService.getCircuits().subscribe((data:any) => {
+    this.circuitsService.getCircuits().subscribe(data => {
       this.circuits = data;
       this.loading = false;
     });
