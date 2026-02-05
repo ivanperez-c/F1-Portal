02cc63433ac390b6faa7771fc7e7cc75747c801a
IvanPerez <64197858+ivanperez-c@users.noreply.github.com>
2026-02-05 16:32:11 +0100

    datos de equipos para administrador

--- frontend/src/app/features/team/team-layout/team-layout.ts
+++ frontend/src/app/features/team/team-layout/team-layout.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-circuit-management',
@@ -10,4 +11,9 @@ import { RouterModule } from '@angular/router';
 })
 export class TeamLayoutComponent {
 
+  private authService = inject(AuthService);
+  get isTeamManager(): boolean {
+    const user = this.authService.getUser();
+    return user?.rol === 'responsable_equipo'; 
+  }
 }
